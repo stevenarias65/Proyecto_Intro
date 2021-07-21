5a25a8b5158983ba59dbfe44c510e8972920a047
stevenarias65 <stevenarias65@gmail.com>
2021-07-20 20:06:04 -0600

    Otra Ventana
    Se agrego otra ventana

--- Inicio.py
+++ Inicio.py
@@ -7,6 +7,17 @@ ventana.title("Prueba Github")
 etiqueta = Label(ventana, text="Ejemplo de como subir cosas en git").pack()
 etiqueta = Label(ventana, text="Etiqueta creada por juan").pack()
 
-botton = Button(ventana, text="Click aqui").pack()
+
+def abriVentana():
+    global root;
+    root = Toplevel(ventana) 
+
+    
+botton = Button(ventana, text="Click aqui", command=abriVentana).pack()
+
+
+
+    
+
 ventana.mainloop()
 
