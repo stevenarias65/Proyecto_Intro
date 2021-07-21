8312479475de50a1f68d00efe839a980d2bff6e7
stevenarias65 <stevenarias65@gmail.com>
2021-07-20 20:03:08 -0600

    agregue boton
    Se creo un boton

--- Inicio.py
+++ Inicio.py
@@ -6,5 +6,7 @@ ventana.title("Prueba Github")
 
 etiqueta = Label(ventana, text="Ejemplo de como subir cosas en git").pack()
 etiqueta = Label(ventana, text="Etiqueta creada por juan").pack()
+
+botton = Button(ventana, text="Click aqui").pack()
 ventana.mainloop()
 
