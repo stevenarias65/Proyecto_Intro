a294f7dbbf7498bbdd4077b7284d8cd12be55ecc
JTS628 <jj628@hotmail.com>
2021-07-20 19:59:00 -0600

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -5,4 +5,6 @@ ventana = Tk()
 ventana.title("Prueba Github")
 
 etiqueta = Label(ventana, text="Ejemplo de como subir cosas en git").pack()
+etiqueta = Label(ventana, text="Etiqueta creada por juan").pack()
 ventana.mainloop()
+
